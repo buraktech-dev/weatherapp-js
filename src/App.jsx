bf1d814ee9fe20ab4f7b5506278e51ebf69c4d32
Burak Tech <buraktechdev@gmail.com>
2024-03-03 23:04:51 +0100

    added a search button

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ function App() {
       <div className="card">
         <div className="search-item">
           <input placeholder="Search for a city" className="searchbar"></input>
+          <button className="searchbutton">Search</button>
         </div>
         <div className="main-item">
           <h1 className="degree">24</h1>
